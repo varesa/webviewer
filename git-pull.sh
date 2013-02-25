203eea586c519cf48fe86ca504124a016d2c4c30
Esa Varemo <esa@kuivanto.fi>
2013-02-25 20:22:25 +0200

    Modified scripts to be a bit more verbose

--- git-pull.sh
+++ git-pull.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo Pulling master repo
 git pull origin master
+echo Pulling submodules
 git submodule foreach pull origin master
--- git-push.sh
+++ git-push.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo Pushing to master repo
 git push origin master
+echo Pushing to submodules
 git submodule foreach push origin master
